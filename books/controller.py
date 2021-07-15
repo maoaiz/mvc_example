afa05088d38caff03aff145119943ed3b5eaf0de
Mauricio Aizaga <mauricioaizaga@gmail.com>
2021-07-15 16:21:02 -0500

    Se separa la consulta del controlador al modelo

--- books/controller.py
+++ books/controller.py
@@ -5,12 +5,14 @@ from .models import Book
 def list(requests):
     '''Obtener todos los libros'''
 
-    book_list = Book.objects.all()
-    return render(requests, 'list.html', locals())
+    book_list = Book.get_all_books()  # acceso a los datos
+
+    return render(requests, 'list.html', locals())  # definición de la vista (template) a mostrar
 
 
 def book(requests, pk):
     '''Obtener la información del libro con pk definido'''
 
-    book_obj = Book.objects.get(pk=pk)
-    return render(requests, 'book.html', locals())
+    book_obj = Book.get_book(pk=pk)  # acceso a los datos
+
+    return render(requests, 'book.html', locals())  # definición de la vista (template) a mostrar
--- books/models.py
+++ books/models.py
@@ -4,3 +4,11 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=300)
     writer = models.CharField(max_length=300)
+
+    @staticmethod
+    def get_all_books():
+        return Book.objects.all()
+
+    @staticmethod
+    def get_book(pk):
+        return Book.objects.get(pk=pk)
